178cb447f0a3483d4ce7a08240eb397299ddcbd4
alexander voeler <avoeler@gmail.com>
2024-02-21 14:25:31 +0900

    Some small changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_20_083023) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_21_051539) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,13 +43,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_20_083023) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.string "status"
     t.date "start_date"
     t.date "end_date"
     t.bigint "user_id", null: false
     t.bigint "offer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "Pending"
     t.index ["offer_id"], name: "index_bookings_on_offer_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
@@ -87,7 +87,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_20_083023) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
