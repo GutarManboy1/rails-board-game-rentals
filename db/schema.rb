da6eb9cab2a08e8b3fd372ee8d400ad376e1cd5b
Jason Sturgeon <zergyjanus@gmail.com>
2024-02-22 14:33:12 +0900

    added Devise files to style

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_21_051539) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_22_030005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_21_051539) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "string_url"
+  end
+
+  create_table "logins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "offers", force: :cascade do |t|
