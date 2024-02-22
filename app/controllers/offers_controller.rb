f55fa07b1b948d1e3bbc17e3302131601a99a818
Jason Sturgeon <zergyjanus@gmail.com>
2024-02-22 12:29:53 +0900

    add new offer functionality working

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,13 +1,29 @@
 class OffersController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def show
     @booking = Booking.new
     @offer = Offer.find(params[:id])
   end
 
   def new
+    @offer = Offer.new
   end
 
   def create
+    @offer = Offer.new(offer_params)
+    @offer.user = current_user
+    @offer.pending_request = false
+    if @offer.save
+      redirect_to bookings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-  
+
+private
+
+  def offer_params
+    params.require(:offer).permit(:comment, :price, :picture, :user, :game_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :games, only: [:index, :show] do
-    resources :offers, only: [:new, :create]
-  end
+  resources :games, only: [:index, :show]
 
-
-  resources :offers, only: [:edit, :update, :destroy, :show] do
+  resources :offers, only: [:edit, :update, :destroy, :show, :new, :create] do
     resources :bookings, only: [:create]
   end
 
